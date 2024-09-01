5a6a21bd4394a339185d61762cfdc28307b32910
bakhtiar-mazrur <91288326+bakhtiar-mazrur@users.noreply.github.com>
2024-09-01 15:12:28 +0600

    Initial commit

---
+++ src/test/java/com/mt007/pharmamanage/controller/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt007.pharmamanage.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
